d233801de0d6e0577b2c0172992252f24ac39bed
Dennis Christ <dennis.christ@stud-mail.uni-wuerzburg.de>
2017-10-28 13:42:45 +0200

    Renamed JAVA controller and JSP files

--- src/main/java/de/uniwue/controller/GroundTruthCorrection.java
+++ src/main/java/de/uniwue/controller/GroundTruthCorrection.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HelloWorldController {
+public class GroundTruthCorrection {
     String message = "Welcome to Spring MVC!";
  
     @RequestMapping("/")
     public ModelAndView showMessage(
         @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 
-        ModelAndView mv = new ModelAndView("helloworld");
+        ModelAndView mv = new ModelAndView("groundtruthcorrection");
         mv.addObject("message", message);
         mv.addObject("name", name);
         return mv;
